f9cab69f819bfaad868820cdcd8ea371ce2fcc9b
Reda <mmouhamed@octo.com>
2013-07-22 17:22:21 +0200

    renamed project to OTAppaloosaDemo

---
+++ Example/OTAppaloosaDemo/OTAppaloosaDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OTInAppFeedback
+//
+//  Created by Maxence Walbrou on 06/02/13.
+//  Copyright (c) 2013 OCTO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OTAppDelegate class]));
+    }
+}
