69ad67c30efc2e7a8f47ca2c83bfef5c61faa541
cpo <cpo@octo.com>
2013-08-29 10:45:42 +0200

    new service to check application update

--- OTAppaloosa/Classes/Service/OTAppaloosaService.h
+++ OTAppaloosa/Classes/Service/OTAppaloosaService.h
@@ -1,6 +1,19 @@
+// Copyright 2013 OCTO Technology
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //
 //  OTAppaloosaService.h
-//  Apploosa-SDK-HOME
 //
 //  Created by Cedric Pointel on 06/08/13.
 //  Copyright (c) 2013 OCTO. All rights reserved.
@@ -30,4 +43,13 @@
                                    storeToken:(NSString *)storeToken
                                   withSuccess:(void (^)(OTAppaloosaApplication *application))success
                                       failure:(void (^)(NSString *message))failure;
+
+/**************************************************************************************************/
+#pragma mark - Application Update
+
+- (void)checkApplicationUpdateWithStoreId:(NSString *)storeId
+                                 bundleId:(NSString *)bundleId
+                               storeToken:(NSString *)storeToken
+                              withSuccess:(void (^)(BOOL shouldUpdate))success
+                                  failure:(void (^)(NSString *message))failure;
 @end
--- OTAppaloosa/Classes/Service/OTAppaloosaService.m
+++ OTAppaloosa/Classes/Service/OTAppaloosaService.m
@@ -1,6 +1,19 @@
+// Copyright 2013 OCTO Technology
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //
 //  OTAppaloosaService.m
-//  Apploosa-SDK-HOME
 //
 //  Created by Cedric Pointel on 06/08/13.
 //  Copyright (c) 2013 OCTO. All rights reserved.
@@ -47,10 +60,10 @@
 #pragma mark - Authorization
 
 - (void)checkAuthorizationsWithStoreId:(NSString *)storeId
-                                bundleId:(NSString *)bundleId
-                           storeToken:(NSString *)storeToken
-                          withSuccess:(void (^)(void))success
-                              failure:(void (^)(OTApplicationAuthorization *appAuthorization))failure
+                              bundleId:(NSString *)bundleId
+                            storeToken:(NSString *)storeToken
+                           withSuccess:(void (^)(void))success
+                               failure:(void (^)(OTApplicationAuthorization *appAuthorization))failure;
 {
     if ([[Reachability reachabilityForInternetConnection] isReachable])
     {
@@ -147,8 +160,6 @@
                                        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
                                                                                             options:kNilOptions
                                                                                               error:&error];
-                                       
-                                       AppaloosaLog(@"json = %@",json);
                                        if ((error == nil) && json)
                                        {
                                            OTAppaloosaApplication *application = [[OTAppaloosaApplication alloc] initWithJsonDictionary:json];
@@ -173,6 +184,62 @@
                                        });
                                    }
          
+                               }];
+    }
+}
+
+/**************************************************************************************************/
+#pragma mark - Application Update
+
+- (void)checkApplicationUpdateWithStoreId:(NSString *)storeId
+                                 bundleId:(NSString *)bundleId
+                               storeToken:(NSString *)storeToken
+                              withSuccess:(void (^)(BOOL shouldUpdate))success
+                                  failure:(void (^)(NSString *message))failure;
+{
+    if ([[Reachability reachabilityForInternetConnection] isReachable])
+    {
+        NSString *urlString = [OTAppaloosaUrlHelper urlForApplicationUpdateWithStoreId:storeId
+                                                                              bundleId:bundleId
+                                                                            storeToken:storeToken];
+        
+        NSURL *url = [NSURL URLWithString:urlString];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+       
+        AppaloosaLog(@"Check Application Update from %@",urlString);
+
+        [NSURLConnection sendAsynchronousRequest:request
+                                           queue:self.queue
+                               completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+                                   
+                                   if (!error)
+                                   {
+                                       NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+                                                                                            options:kNilOptions
+                                                                                              error:&error];
+                                       
+                                       if ((error == nil) && json)
+                                       {
+                                           if(success)
+                                           {
+                                               dispatch_sync(dispatch_get_main_queue(), ^{
+                                                   
+                                                   success(NO);
+                                               });
+                                               return;
+                                           }
+                                       }
+                                   }
+                                   
+                                   if(failure)
+                                   {
+                                       dispatch_sync(dispatch_get_main_queue(), ^{
+                                           
+                                           AppaloosaLog(@"Unable to check Application Updates");
+                                           failure(@"An error has occurred");
+                                       });
+                                   }
+                                   
                                }];
     }
 }
