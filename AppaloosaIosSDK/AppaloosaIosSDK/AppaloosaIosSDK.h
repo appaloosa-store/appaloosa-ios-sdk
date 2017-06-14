4f08b952d6b980aeadf899798096a6f0f7247817
Elise EL BEZE <eelbeze@octo.com>
2017-06-14 15:10:19 +0200

    Add xcodeproj

---
+++ AppaloosaIosSDK/AppaloosaIosSDK/AppaloosaIosSDK.h
@@ -0,0 +1,19 @@
+ //
+//  AppaloosaIosSDK.h
+//  AppaloosaIosSDK
+//
+//  Created by El BEZE on 14/06/2017.
+//  Copyright © 2017 El BEZE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppaloosaIosSDK.
+FOUNDATION_EXPORT double AppaloosaIosSDKVersionNumber;
+
+//! Project version string for AppaloosaIosSDK.
+FOUNDATION_EXPORT const unsigned char AppaloosaIosSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppaloosaIosSDK/PublicHeader.h>
+
+
